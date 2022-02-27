900df7402fdaa7972a6878e590e1bad6a6e1eef6
borobor <bornajurisa@gmail.com>
2022-02-27 18:32:30 +0100

    Display choices and results on webpage

--- js/script.js
+++ js/script.js
@@ -11,36 +11,41 @@ function compPlay() {
   } else return "scissors";
 }
 
+let choices = document.createElement('p');
+let results = document.createElement('p');
+let div = document.querySelector('#results');
+div.appendChild(choices);
+div.appendChild(results);
+
 function PlayRound() {
   let compChoice = compPlay();
 	let userChoice = this.id;
-  console.log(`User choice: ${userChoice}`);
-  console.log(`Computer choice: ${compChoice}`);
+  choices.innerText = `User choice: ${userChoice}\n Computer choice: ${compChoice}\n`;
   if (userChoice == compChoice) {
-    console.log("It's a tie!");
+    results.innerText = "It's a tie!";
   }
   else if (userChoice == "rock") {
     if (compChoice == "paper") {
       compScore++;
-      console.log("You lose! Paper beats rock!");
+      results.innerText ="You lose! Paper beats rock!";
     } else { userScore++;
-    console.log("You win! Rock beats scissors!");
+    results.innerText = "You win! Rock beats scissors!";
 		}
   } 
 	else if (userChoice == "paper") {
     if (compChoice == "rock") {
       userScore++;
-      console.log("You win! Paper beats rock!");
+      results.innerText = "You win! Paper beats rock!";
     } else { compScore++;
-    console.log("You lose! Scissors beat paper!");
+    results.innerText = "You lose! Scissors beat paper!";
 		}
   } 
 	else {
   	if (compChoice == "rock") {
     	compScore++;
-    	console.log("You lose! Rock beats scissors!");
+    	results.innerText = "You lose! Rock beats scissors!";
   	} else { userScore++;
-  	console.log("You win! Scissors beat paper!");
+  	results.innerText = "You win! Scissors beat paper!";
 		}
 	}
 }
