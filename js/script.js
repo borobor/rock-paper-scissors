ce3f71a617c11218e0bb1874cbc05dd287c35b0c
borobor <bornajurisa@gmail.com>
2022-02-27 22:21:26 +0100

    Add round count and final score

--- js/script.js
+++ js/script.js
@@ -11,63 +11,72 @@ function compPlay() {
   } else return "scissors";
 }
 
-let choices = document.createElement('p');
-let results = document.createElement('p');
-let div = document.querySelector('#results');
+let choices = document.createElement("p");
+let results = document.createElement("p");
+let div = document.querySelector("#results");
 div.appendChild(choices);
 div.appendChild(results);
 
-function PlayRound() {
+function PlayRound(event) {
   let compChoice = compPlay();
-	let userChoice = this.id;
+  let userChoice = event.target.id;
   choices.innerText = `User choice: ${userChoice}\n Computer choice: ${compChoice}\n`;
   if (userChoice == compChoice) {
     results.innerText = "It's a tie!";
-  }
-  else if (userChoice == "rock") {
+		roundCount--;
+  } else if (userChoice == "rock") {
     if (compChoice == "paper") {
       compScore++;
-      results.innerText ="You lose! Paper beats rock!";
-    } else { userScore++;
-    results.innerText = "You win! Rock beats scissors!";
-		}
-  } 
-	else if (userChoice == "paper") {
+      results.innerText = "You lose! Paper beats rock!";
+    } else {
+      userScore++;
+      results.innerText = "You win! Rock beats scissors!";
+    }
+  } else if (userChoice == "paper") {
     if (compChoice == "rock") {
       userScore++;
       results.innerText = "You win! Paper beats rock!";
-    } else { compScore++;
-    results.innerText = "You lose! Scissors beat paper!";
-		}
-  } 
-	else {
-  	if (compChoice == "rock") {
-    	compScore++;
-    	results.innerText = "You lose! Rock beats scissors!";
-  	} else { userScore++;
-  	results.innerText = "You win! Scissors beat paper!";
-		}
-	}
+    } else {
+      compScore++;
+      results.innerText = "You lose! Scissors beat paper!";
+    }
+  } else {
+    if (compChoice == "rock") {
+      compScore++;
+      results.innerText = "You lose! Rock beats scissors!";
+    } else {
+      userScore++;
+      results.innerText = "You win! Scissors beat paper!";
+    }
+  }
 }
+
+let score = document.createElement("p");
+div.appendChild(score);
+let endResult = document.createElement("p");
+div.appendChild(endResult);
+let roundCount = 0;
 
 function theGame() {
-  for (let i = 1; i <= 5; i++) {
-    console.log(`----ROUND ${i}----`);
-    PlayRound();
-    console.log(`Man vs. Machine: ${userScore} - ${compScore}`);
-  }
-  if (userScore == compScore) {
-		console.log("This one was tight! It's a tie");
+  PlayRound(event);
+	roundCount++;
+	if (roundCount <= 5) {
+		score.innerText = `----ROUND ${roundCount}----\n`;
+		score.innerText += `Man vs. Machine: ${userScore} - ${compScore}`;
 	}
-	else {
-		(userScore > compScore) ?
-			console.log("You won!!") :
-			console.log("You lost :(");
-	}
+	if (roundCount == 5) {
+		if (userScore == compScore) {
+			endResult.innerText += "\nThis one was tight! It's a tie";
+		} else {
+			userScore > compScore
+				? (endResult.innerText += "\nYou won!!")
+				: (endResult.innerText += "\nYou lost :(");
+			}		
+	}  
 }
 
-buttons = document.querySelectorAll('button');
+buttons = document.querySelectorAll("button");
 
-buttons.forEach(button => {
-	button.addEventListener('click', PlayRound);
-})
+buttons.forEach((button) => {
+  button.addEventListener("click", theGame);
+});
